c80c272360a6660e1ede2ffc3f5ad6927babe5bd
Laurin <lafisher@alaska.edu>
2020-03-30 15:57:42 -0800

    Giving up on dynamic code execution of cutscene out of json file.

--- William RPG/Assets/Scripts/Overworld/Action.cs
+++ William RPG/Assets/Scripts/Overworld/Action.cs
@@ -7,12 +7,25 @@ using System.Reflection;
 public class Action {
 
 	public string function; 
-	public System.Object [] parameters; 
+	public string [] parametersJSON; 
 
 	public void DoAction(){
-		Debug.Log(parameters[0]); 
+		Debug.Log(parametersJSON); 
+		//get the parameter types from the method 
 		var method = typeof(Data).GetMethod(function, BindingFlags.Public | BindingFlags.Static); 
-		if(method == null) throw new System.MissingMethodException($"Could not resolve function name '{function}'."); 
+		if(method == null) throw new System.MissingMethodException($"Could not resolve function name '{function}'.");
+		ParameterInfo[] pars = method.GetParameters(); 
+
+		//Create objects out of the parameter values in json
+		Object [] parameters = new Object[parametersJSON.Length]; 
+		for(int i=0; i<pars.Length; i++){
+			Debug.Log(pars[i].ParameterType); 
+			//deserialize the json to get the arguments to create the object with
+			var obj = JsonUtility.FromJson<pars[i].ParameterType>(parametersJSON[i]); 
+			parameters[i] = obj; 
+		}
+
+		//Call the function with the parameters 
 		method.Invoke(null, parameters); 
 	}
 }
